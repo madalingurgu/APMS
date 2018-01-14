73099b44750c583b278182f18e6cee1973f24618
madalingurgu <gurgu.madalin@gmail.com>
2018-01-14 17:41:22 +0000

    models trials - foreingkeys tests

--- APMS/apps/orders/admin.py
+++ APMS/apps/orders/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from orders.models import Order
+from orders.models import Order, Entry
 
 class OrderAdmin(admin.ModelAdmin):
     model = Order
-    list_display = ('order_number', 'customer_name', 'post_date',)
+    model = Entry
+    list_display = ('order_number', 'customer_name', 'post_date', 'entry',)
     prepopulated_fields = {'slug': ('order_number',)}
+
+class EntryAdmin(admin.ModelAdmin):
+    model = Entry
+    list_display = ('order_type',)
+
 
 # Register your models here.
 admin.site.register(Order, OrderAdmin)
+admin.site.register(Entry, EntryAdmin)
+
+
--- APMS/apps/orders/models.py
+++ APMS/apps/orders/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Entry(models.Model):
+
+    order_type = models.CharField(max_length=50)
+     
+        
+    def __str__(self):
+        return self.order_type
+    
+    
+
 class Order(models.Model):
     order_number = models.IntegerField(unique=True)
     drawing_number = models.CharField(max_length=20)
@@ -9,6 +19,8 @@ class Order(models.Model):
     
     slug = models.SlugField(unique=True)
     post_date = models.DateTimeField('date posted')
+    
+    entry = models.ForeignKey(Entry, null=True, on_delete=models.SET_NULL)
     
     def __str__(self):
         return self.slug
