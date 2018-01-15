ba753ba7e1ff48cf6913157a494c1884b02f9198
madalingurgu <gurgu.madalin@gmail.com>
2018-01-15 14:34:59 +0000

    choises atempt

--- APMS/apps/orders/admin.py
+++ APMS/apps/orders/admin.py
@@ -1,19 +1,26 @@
 from django.contrib import admin
-from orders.models import Order, Entry
+from orders.models import Order, Request, Sample
 
 class OrderAdmin(admin.ModelAdmin):
     model = Order
-    model = Entry
-    list_display = ('order_number', 'customer_name', 'post_date', 'entry',)
+    list_display = ('order_number', 'customer_name', 'post_date',)
     prepopulated_fields = {'slug': ('order_number',)}
+    
+class SampleAdmin(admin.ModelAdmin):
+    model = Sample
+    list_display = ('description',)    
 
-class EntryAdmin(admin.ModelAdmin):
-    model = Entry
-    list_display = ('order_type',)
+
+class RequestAdmin(admin.ModelAdmin):
+    model = Request
+    list_display = ('order', 'sample', 'order_by',)
+    
+
 
 
 # Register your models here.
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Entry, EntryAdmin)
+admin.site.register(Request, RequestAdmin)
+admin.site.register(Sample, SampleAdmin)
 
 
--- APMS/apps/orders/models.py
+++ APMS/apps/orders/models.py
@@ -2,25 +2,39 @@ from django.db import models
 
 # Create your models here.
 
-class Entry(models.Model):
 
-    order_type = models.CharField(max_length=50)
-     
-        
-    def __str__(self):
-        return self.order_type
-    
-    
+
 
 class Order(models.Model):
     order_number = models.IntegerField(unique=True)
     drawing_number = models.CharField(max_length=20)
     customer_name = models.CharField(max_length=50)
-    
-    slug = models.SlugField(unique=True)
     post_date = models.DateTimeField('date posted')
-    
-    entry = models.ForeignKey(Entry, null=True, on_delete=models.SET_NULL)
-    
+    slug = models.SlugField(unique=True)
+
     def __str__(self):
         return self.slug
+        
+class Sample(models.Model):
+    description = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.description
+    
+    
+FAVE_CHOICES = ( 
+    ('A','Order'),
+    ('C','Sample'),
+)
+    
+        
+class Request(models.Model):
+
+    type_of_fave = models.CharField( max_length=1, 
+        choices=FAVE_CHOICES, null=True, on_delete=models.SET_NULL)
+#    order = models.ForeignKey(Order, null=True, on_delete=models.SET_NULL)
+#    sample = models.ForeignKey(Sample, null=True, on_delete=models.SET_NULL)
+    order_by = models.CharField(max_length=50, null=True)
+
+#    def __str__(self):
+#        return self.order
