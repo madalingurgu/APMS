e254d86abe66b8f65d26bc51e3a4360f87a3046f
madalingurgu <gurgu.madalin@gmail.com>
2018-01-19 14:07:57 +0000

    crispy forms customization in progress

--- APMS/apps/orders/forms.py
+++ APMS/apps/orders/forms.py
@@ -1,8 +1,33 @@
 from django.forms import ModelForm
 from django import forms
 from orders.models import Staff, Product, Customer, Order, Engineering, Request
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Div, Fieldset, Field
+from django.urls import reverse
 
-class OrderForm(ModelForm):
+
+class OrderForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-order-form'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('order_create')
+        self.helper.add_input(Submit('submit', 'Save', css_class='btn-success'))
+        # self.helper.label_class = 'col-md-2'
+        self.helper.layout = Layout(
+            Field('project_no', style="color: brown;", 
+                placeholder='Please input the project number!',),
+            Field('r_type', style="color: brown;",),
+            Fieldset('', 'product', 'estimate', style="color: brown;"),
+                
+            
+            # HTML("""<h3>Create new customers account</h3>"""),
+            # Row(Field('first_name',),),
+            # Field('project_no', placeholder='Your first name', css_class="some-class")
+            # Div('last_name', title="Your last name")
+            )
     class Meta:
         model = Request
         labels = {
@@ -15,17 +40,8 @@ class OrderForm(ModelForm):
                   'estimate',
                   'status',
         )
+        # widgets = {
+        #     'project_no':TextInput(attrs={'size':'70','cols': 10, 'rows': 20}),  
+        # }
 
-
-    # def __init__(self, *args, **kwargs):
-    #     super(OrderForm, self).__init__(*args, **kwargs)
-    #     self.fields['estimate'].widget.attrs.update({
-    #         'class' : 'form-control'
-    #     })
     
-        widgets = {
-            'estimate': forms.TextInput(attrs={'type': 'email', 'class': 'form-control'}),
-            'project_no': forms.TextInput(attrs={'class': 'form-control'}),
-            'r_type': forms.Select(attrs={'class': 'form-control'}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
-        }
